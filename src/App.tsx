65a38cde4748b97d7d6783643aecd7b955928541
Andre Delfs <andrecrampie@gmail.com>
2021-03-26 16:04:58 +0100

    Es wurden Vertikale und Horizontale Wand Komponenten hinzugefügt.

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,32 @@ import Coin from "./Components/Coin";
 import Empy from "./Components/Empty";
 import Wall from "./Components/Wall";
 import Holl from "./Components/HorizontalWall";
+import Vell from "./Components/VerticalWall";
 import Spielfeld from "./Components/Spielfeld";
 import Hack from "./Components/Hackman";
+
 
 // prettier-ignore
 let fields: React.FC[][] = [
   [Wall, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Wall],
-  [Wall, Coin, Coin, Coin, Coin, Wall, Coin, Coin, Coin, Coin, Wall, Coin, Coin, Coin, Coin, Wall, Coin, Coin, Coin, Coin, Wall],
-  [Wall, Coin, Wall, Wall, Coin, Wall, Coin, Wall, Wall, Coin, Coin, Coin, Wall, Wall, Coin, Wall, Coin, Wall, Wall, Coin, Wall],
-  [Wall, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Wall, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Wall],
-  [Wall, Wall, Coin, Wall, Wall, Wall, Wall, Wall, Coin, Wall, Wall, Wall, Coin, Wall, Wall, Wall, Wall, Wall, Coin, Wall, Wall],
-  [Wall, Wall, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Wall, Wall],
-  [Wall, Wall, Coin, Wall, Coin, Wall, Wall, Coin, Wall, Wall, Empy, Wall, Wall, Coin, Wall, Wall, Coin, Wall, Coin, Wall, Wall],
-  [Empy, Empy, Coin, Wall, Coin, Wall, Wall, Coin, Wall, Empy, Empy, Empy, Wall, Coin, Wall, Wall, Coin, Wall, Coin, Empy, Empy],
-  [Wall, Wall, Coin, Wall, Coin, Wall, Wall, Coin, Wall, Empy, Empy, Empy, Wall, Coin, Wall, Wall, Coin, Wall, Coin, Wall, Wall],
-  [Empy, Empy, Coin, Wall, Coin, Wall, Wall, Coin, Wall, Empy, Empy, Empy, Wall, Coin, Wall, Wall, Coin, Wall, Coin, Empy, Empy],
-  [Wall, Wall, Coin, Wall, Coin, Wall, Wall, Coin, Wall, Wall, Wall, Wall, Wall, Coin, Wall, Wall, Coin, Wall, Coin, Wall, Wall],
-  [Wall, Coin, Coin, Coin, Coin, Coin, Coin, Hack, Coin, Coin, Wall, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Wall],
-  [Wall, Coin, Wall, Wall, Coin, Wall, Coin, Wall, Wall, Coin, Coin, Coin, Wall, Wall, Coin, Wall, Coin, Wall, Wall, Coin, Wall],
-  [Wall, Coin, Coin, Coin, Coin, Wall, Coin, Coin, Coin, Coin, Wall, Coin, Coin, Coin, Coin, Wall, Coin, Coin, Coin, Coin, Wall],
+  [Vell, Coin, Coin, Coin, Coin, Vell, Coin, Coin, Coin, Coin, Vell, Coin, Coin, Coin, Coin, Vell, Coin, Coin, Coin, Coin, Vell],
+  [Vell, Coin, Holl, Holl, Coin, Vell, Coin, Holl, Holl, Coin, Coin, Coin, Holl, Holl, Coin, Vell, Coin, Holl, Holl, Coin, Vell],
+  [Vell, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Vell, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Vell],
+  [Vell, Wall, Coin, Holl, Holl, Holl, Holl, Holl, Coin, Holl, Holl, Holl, Coin, Holl, Holl, Holl, Holl, Holl, Coin, Wall, Vell],
+  [Vell, Vell, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Vell, Vell],
+  [Wall, Wall, Coin, Vell, Coin, Wall, Wall, Coin, Wall, Holl, Empy, Holl, Wall, Coin, Wall, Wall, Coin, Vell, Coin, Wall, Wall],
+  [Empy, Empy, Coin, Vell, Coin, Vell, Vell, Coin, Vell, Empy, Empy, Empy, Vell, Coin, Vell, Vell, Coin, Vell, Coin, Empy, Empy],
+  [Holl, Holl, Coin, Vell, Coin, Vell, Vell, Coin, Vell, Empy, Empy, Empy, Vell, Coin, Vell, Vell, Coin, Vell, Coin, Holl, Holl],
+  [Empy, Empy, Coin, Vell, Coin, Vell, Vell, Coin, Vell, Empy, Empy, Empy, Vell, Coin, Vell, Vell, Coin, Vell, Coin, Empy, Empy],
+  [Wall, Holl, Coin, Vell, Coin, Wall, Wall, Coin, Wall, Holl, Holl, Holl, Wall, Coin, Wall, Wall, Coin, Vell, Coin, Holl, Wall],
+  [Vell, Coin, Coin, Coin, Coin, Coin, Coin, Hack, Coin, Coin, Vell, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Vell],
+  [Vell, Coin, Holl, Holl, Coin, Vell, Coin, Holl, Holl, Coin, Coin, Coin, Holl, Holl, Coin, Vell, Coin, Holl, Holl, Coin, Vell],
+  [Vell, Coin, Coin, Coin, Coin, Vell, Coin, Coin, Coin, Coin, Vell, Coin, Coin, Coin, Coin, Vell, Coin, Coin, Coin, Coin, Vell],
   [Wall, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Holl, Wall]
 ]
 
 const App: React.FC = () => {
-  return <div className="tester"><Spielfeld fields={fields} /></div>;
+  return <Spielfeld fields={fields} />;
 };
 
 export default App;
