b8ca978827dc889a4bb73e7a059b61c1daf4f189
Hartmann <Felix.Hartmann@hauertmann.net>
2021-06-01 12:03:12 +0200

    Snacks für hackman hinzugefügt

--- src/Components/Spielfeld.tsx
+++ src/Components/Spielfeld.tsx
@@ -21,6 +21,7 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import Koordinate from "../Types/Koordinate";
 import Richtung from "../Types/Richtung";
+import Snack from "./Snack";
 
 interface ISpielfeldProps {
   fields: React.FC[][];
@@ -46,6 +47,7 @@ const renderComponent = (component: React.FC, key: number) => {
   else if (component === CornerRB) return <CornerRB key={key} />;
   else if (component === Coin) return <Coin key={key} />;
   else if (component === Hackman) return <Hackman key={key} />;
+  else if (component === Snack) return <Snack key={key} />;
   else if (component === Empty) return <Empty key={key} />;
   else return undefined;
 };
