7cba6e09bb18054efa9b92cc59afb942754f7987
Maurice Eberle <Maurice.Eberle@hauertmann.net>
2021-06-07 16:53:32 +0200

    Mund und Auge des Hackman werden wieder angezeigt

--- src/Components/Hackman.tsx
+++ src/Components/Hackman.tsx
@@ -2,7 +2,10 @@
 const Hackman: React.FC = () => {
     return (
       <div className="field">
-        <div className="hackman"></div>
+        <div className="hackman">
+          <div className="eye eye-move" id="eye"></div>
+          <div className="mouth mouth-open" id="mouth"></div>
+        </div>
       </div>
     );
   };
