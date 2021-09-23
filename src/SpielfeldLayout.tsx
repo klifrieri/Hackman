12d18ee2cfb0afd817efd6b5f1466a3dc8089129
Maurice Eberle <Maurice.Eberle@hauertmann.net>
2021-09-23 16:08:37 +0200

    ...

--- src/SpielfeldLayout.tsx
+++ src/SpielfeldLayout.tsx
@@ -20,6 +20,8 @@ import Gei1 from "./Components/GameFieldComponent/GhostComponents/Ghost1";
 import Gei2 from "./Components/GameFieldComponent/GhostComponents/Ghost2";
 import Gei3 from "./Components/GameFieldComponent/GhostComponents/Ghost3";
 import Gei4 from "./Components/GameFieldComponent/GhostComponents/Ghost4";
+import Bloc from "./Components/GameFieldComponent/FieldComponents/Path/Block";
+
 import React from 'react';
 
 const SpielfeldLayout = () => {
@@ -30,7 +32,7 @@ const SpielfeldLayout = () => {
         [Vell, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Vwts, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Vell],
         [Tpls, Cort, Coin, Hwls, Holl, Holl, Holl, Hwrs, Coin, Hwls, Tpto, Hwrs, Coin, Hwls, Holl, Holl, Holl, Hwrs, Coin, Colt, Tprs],
         [Vell, Vell, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Snac, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Coin, Vell, Vell],
-        [Colb, Corb, Coin, Vwts, Coin, Colt, Cort, Coin, Colt, Hwrs, Coin, Hwls, Cort, Coin, Colt, Cort, Coin, Vwts, Coin, Colb, Corb],
+        [Colb, Corb, Coin, Vwts, Coin, Colt, Cort, Coin, Colt, Hwrs, Bloc, Hwls, Cort, Coin, Colt, Cort, Coin, Vwts, Coin, Colb, Corb],
         [Empy, Empy, Coin, Vell, Coin, Vell, Vell, Coin, Vell, Gei1, Empy, Gei2, Vell, Coin, Vell, Vell, Coin, Vell, Coin, Empy, Empy],
         [Hwls, Hwrs, Coin, Vell, Coin, Vell, Vell, Coin, Vell, Empy, Empy, Empy, Vell, Coin, Vell, Vell, Coin, Vell, Coin, Hwls, Hwrs],
         [Empy, Empy, Coin, Vell, Coin, Vell, Vell, Coin, Vell, Gei3, Empy, Gei4, Vell, Coin, Vell, Vell, Coin, Vell, Coin, Empy, Empy],
