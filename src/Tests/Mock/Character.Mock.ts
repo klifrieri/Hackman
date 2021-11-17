3b01e154d8eadeffc6ea766af3698ccb5afcdd6f
Andre Delfs <andrecrampie@gmail.com>
2021-11-17 16:17:26 +0100

    removed unused tests

--- src/Tests/Mock/Character.Mock.ts
+++
@@ -1,8 +0,0 @@
-import HackmanCharacter from "../../Types_Classes/Character/HackmanCharacter";
-import CharacterIdentifier from "../../Types_Classes/Character/Models/CharacterIdentifier";
-
-function GetHackmanByPosition(positionY:number,positionX:number): HackmanCharacter{
-    return new HackmanCharacter(CharacterIdentifier.Hackman,positionY,positionX);
-}
-
-export default GetHackmanByPosition;
--- src/Tests/Mock/GhostCharacter.Mock.ts
+++
@@ -1,9 +0,0 @@
-import GhostCharacter from "../../Types_Classes/Character/Base/GhostCharacter";
-import EasyGhostCharacter from "../../Types_Classes/Character/EasyGhostCharacter";
-import CharacterIdentifier from "../../Types_Classes/Character/Models/CharacterIdentifier";
-
-function GetGhostByPosition(positionY:number,positionX:number): GhostCharacter{
-    return new EasyGhostCharacter(CharacterIdentifier.BlueGhost ,positionY,positionX);
-}
-
-export default GetGhostByPosition;
